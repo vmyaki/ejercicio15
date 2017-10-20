b1544cef73c790c57bb5f33d5acac0df87e8f53d
vmyaki <wonka.vm@gmail.com>
2017-10-20 14:03:46 +0200

    subida a git

---
+++ src/main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+		// Declaramos variables
+		
+		//Mensaje
+		System.out.println("Introduce 2 números y te los ordenaré:");
+		
+		//Creamos objeto
+		Scanner teclado=new Scanner(System.in);
+
+	}
+
+}
